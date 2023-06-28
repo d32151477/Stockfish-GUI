b744be50870eb4ee86eb4e104123f7e0a80716b9
d32151477 <d32151477@gmail.com>
2023-06-28 15:51:09 +0900

    초기 커밋.

---
+++ Stockfish-GUI/Scripts/Sounds.cs
@@ -0,0 +1,24 @@
+﻿using Stockfish_GUI.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockfish_GUI
+{
+    internal static class Sounds
+    {
+        public static SoundPlayer Move;
+        public static SoundPlayer Capture;
+
+        static Sounds()
+        {
+            Move = new SoundPlayer(Resources.move);
+            Capture = new SoundPlayer(Resources.capture);
+        }
+    }
+}
